27d4cad57786ce30e63bcb1a07c87916e65a0348
Dr Nic Williams <drnicwilliams@gmail.com>
2015-09-02 11:36:20 -0700

    Volumes::SnapshotStatus no longer exposes Fog RDS objects

--- lib/aws_snapshots_stannisplugin/volumes/snapshot_status.rb
+++ lib/aws_snapshots_stannisplugin/volumes/snapshot_status.rb
@@ -3,18 +3,15 @@ require 'action_view'
 class Stannis::Plugin::AwsSnapshots::Volumes::SnapshotStatus
   include ActionView::Helpers::DateHelper
 
-  def initialize(fog_compute, description_regexp)
+  def initialize(fog_compute)
     @fog_compute = fog_compute
-    @description_regexp = description_regexp
   end
 
-  def latest_snapshot
-    @fog_compute.snapshots.
-      select {|s| s.description =~ @description_regexp }.
+  def stannis_snapshot_data(description_regexp)
+    snapshot = @fog_compute.snapshots.
+      select {|s| s.description =~ description_regexp }.
       sort {|s1, s2| s1.created_at <=> s2.created_at}.last
-  end
 
-  def stannis_data(snapshot)
     if snapshot
       Stannis::Client::DeploymentData.new(
         "Volume snapshot #{snapshot.description}",
@@ -22,7 +19,7 @@ class Stannis::Plugin::AwsSnapshots::Volumes::SnapshotStatus
       )
     else
       Stannis::Client::DeploymentData.new(
-      "Volume snapshot #{@description_regexp.inspect}", "missing", "down")
+      "Volume snapshot #{description_regexp.inspect}", "missing", "down")
     end
   end
 end
--- spec/volumes_snapshot_status_spec.rb
+++ spec/volumes_snapshot_status_spec.rb
@@ -5,28 +5,22 @@ require "active_support/core_ext/date/calculations"
 describe Stannis::Plugin::AwsSnapshots::Volumes::SnapshotStatus do
   let(:fog_compute) { double(Fog::Compute::AWS::Real) }
   let(:yesterday) { 1.day.ago }
-  subject { Stannis::Plugin::AwsSnapshots::Volumes::SnapshotStatus.new(fog_compute, /^r2 microbosh/) }
+  subject { Stannis::Plugin::AwsSnapshots::Volumes::SnapshotStatus.new(fog_compute) }
 
-  describe 'latest snapshot' do
-    it 'fetches' do
-      snapshots = double(Fog::Compute::AWS::Snapshots)
+  describe 'latest' do
+    before do
       expect(fog_compute).to receive(:snapshots).and_return([
           double(Fog::Compute::AWS::Snapshot, description: "r2 microbosh snapshot two days ago", created_at: 2.days.ago),
           double(Fog::Compute::AWS::Snapshot, description: "r2 microbosh snapshot yesterday", created_at: yesterday),
           double(Fog::Compute::AWS::Snapshot, description: nil, created_at: yesterday),
           double(Fog::Compute::AWS::Snapshot, description: "unrelated", created_at: yesterday),
         ])
-      latest = subject.latest_snapshot
-      expect(latest).to_not be_nil
-      expect(latest.description).to eq("r2 microbosh snapshot yesterday")
-      expect(latest.created_at).to eq(yesterday)
     end
-  end
 
-  describe 'packaging for Stannis::Client' do
     describe 'existing snapshots' do
-      let(:snapshot) { double(Fog::Compute::AWS::Snapshot, description: "r2 microbosh snapshot yesterday", created_at: yesterday) }
-      before { @data = subject.stannis_data(snapshot) }
+      before do
+        @data = subject.stannis_snapshot_data(/^r2 microbosh/)
+      end
       it('for found snapshot') { expect(@data).to_not be_nil }
       it('no validation errors') { expect(@data.validation_errors).to eq([]) }
       it('has label') { expect(@data.label).to eq("Volume snapshot r2 microbosh snapshot yesterday") }
@@ -35,10 +29,12 @@ describe Stannis::Plugin::AwsSnapshots::Volumes::SnapshotStatus do
     end
 
     describe 'no snapshots' do
-      before { @data = subject.stannis_data(nil) }
+      before do
+        @data = subject.stannis_snapshot_data(/unknown/)
+      end
       it('for missing snapshot') { expect(@data).to_not be_nil }
       it('no validation errors') { expect(@data.validation_errors).to eq([]) }
-      it('has label') { expect(@data.label).to eq("Volume snapshot /^r2 microbosh/") }
+      it('has label') { expect(@data.label).to eq("Volume snapshot /unknown/") }
       it('has "missing" value') { expect(@data.value).to eq("missing") }
       it('has indicator') { expect(@data.indicator).to eq("down") }
     end
