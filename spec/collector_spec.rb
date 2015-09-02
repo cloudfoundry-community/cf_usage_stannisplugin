0ff130aefb0ea167363fbcb23c67cabaf803790a
Dr Nic Williams <drnicwilliams@gmail.com>
2015-09-02 10:14:26 -0700

    refactor variables into let()

--- spec/collector_spec.rb
+++ spec/collector_spec.rb
@@ -1,16 +1,19 @@
 require "spec_helper"
 
 describe Stannis::Plugin::AwsSnapshots::Collector do
-  let(:config) { double(Stannis::Plugin::AwsSnapshots::Config) }
+  let(:cf_redis_deployment_config) { {"deployment_name" => "cf"} }
+  let(:redis_deployment_config2) { {"deployment_name" => "redis"} }
+  let(:config) do
+    double(Stannis::Plugin::AwsSnapshots::Config,
+      deployments: [cf_redis_deployment_config, redis_deployment_config2])
+  end
   subject { Stannis::Plugin::AwsSnapshots::Collector.new(config) }
+  let(:cf_data) { double(Stannis::Client::DeploymentData) }
+  let(:redis_data) { double(Stannis::Client::DeploymentData) }
   it 'fetches statuses for each deployment' do
-    cf_redis_deployment_config = {"deployment_name" => "cf"}
-    redis_deployment_config2 = {"deployment_name" => "redis"}
-    cf_data = double(Stannis::Client::DeploymentData)
-    redis_data = double(Stannis::Client::DeploymentData)
-    expect(config).to receive(:deployments).and_return([cf_redis_deployment_config, redis_deployment_config2])
     expect(subject).to receive(:fetch_statuses_for_deployment).with(cf_redis_deployment_config).and_return(cf_data)
     expect(subject).to receive(:fetch_statuses_for_deployment).with(redis_deployment_config2).and_return(redis_data)
     expect(subject.fetch_statuses).to eq([cf_data, redis_data])
   end
+
 end
